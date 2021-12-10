673a6d594ab5210221235cd1fff221dce7dbbfc6
niel63 <placmedia@gmail.com>
2021-12-10 03:33:59 +0100

    ......

--- src/components/content/home/WhatWeOffer.tsx
+++ src/components/content/home/WhatWeOffer.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import { Box, Flex, Heading, List, Image, ListItem, useColorModeValue as mode, VStack, HStack } from '@chakra-ui/react'
-import { MdCheckCircle } from 'react-icons/md'
 
 const ListIcon = <Image src="../image/icons/point.svg" boxSize="16px" objectFit="contain" alt="searchIcon"  />
 
 export default function WhatWeOffer() {
     return (
-        <Box maxW="7xl" mx="auto" bg={mode('skyBlue', 'deepBlue.100')}>
-            <Flex p={["8","16"]} py={["16", "20"]}>
-                <Box  maxW="5xl" px={[0, 20]}>
+        <Box maxW="7xl" mx="auto" bg={mode('skyBlue', 'deepBlue.100')} minH={["800", "0"]}>
+            <Flex p={["8","16"]} py={["16", "28"]}  direction={["column", "row"]} bgImage="url('../image/icons/girlie.svg')" bgPosition={["center 100%", "center right"]} bgRepeat="no-repeat" bgSize={["400px", "750px"]}>
+                <Box px={[0, 20]} pb={["40", "0"]}>
                     <VStack>
                         <Box>
                         <Heading as="h1" fontSize={["18","22"]} fontWeight="medium" color={mode('deepBlue.100', 'skyBlue')}>What we Offer</Heading>
@@ -29,9 +28,7 @@ export default function WhatWeOffer() {
                         </Box>
                     </VStack>
                 </Box>
-                <Box>
                 
-                </Box>
             </Flex>
         </Box>
     )
