66fe38a50f2fbdfc70fea1c4757e8d0b42d50c39
badt0men <placmedia@gmail.com>
2021-12-22 00:41:12 +0100

    ....

--- pages/api/users/allUsers.tsx
+++ pages/api/users/allUsers.tsx
@@ -1,11 +1,15 @@
-// import type { NextApiRequest, NextApiResponse } from 'next'
-// import { getAllUsers } from '../../../models/userModel';
+import type { NextApiRequest, NextApiResponse } from 'next'
+//import { getAllUsers } from '../../../models/userModel';
 
 
 // export default async function (req: NextApiRequest, res: NextApiResponse) {
 //     const data = await getAllUsers
 //     res.status(200).json(data.rows)
 // }
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+    res.status(200).json({"name":"vinod"})
+}
 
 
 // export default async (req:NextApiRequest, res:NextApiResponse) => {
